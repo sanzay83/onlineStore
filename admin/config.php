16c94bdde17aeb6366343198112d663811931083
Sanjay Duwal <sanzay8342258@gmail.com>
2024-04-08 23:36:46 -0500

    removing old project

--- admin/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'u778068254_onlineStore');
-   define('DB_PASSWORD', 'onlineStore123!');
-   define('DB_DATABASE', 'u778068254_onlineStore');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
